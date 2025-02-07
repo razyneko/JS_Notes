292670676659e315868d6f816914f5df8692cda7
razyneko <139580532+INFERNO1211@users.noreply.github.com>
2025-02-08 00:02:47 +0530

    node.js done

---
+++ Backend/node.js
@@ -0,0 +1,50 @@
+// <----------------- What is Node ? ---------------->
+
+// until recently, we could only run js code inside the browser
+// node is a js runtime that executes js code outside the browser
+// same js syntax can be used then to write server side code
+
+// <---------------- What is Node used for ? ---------->
+
+// Web Servers
+// CLI tools
+// Native Apps ( VS code is a Node app)
+// Video Games
+// Drone Software
+
+// <------------- Node REPL ------------>
+
+// read evaluate print loop
+// browser console is a REPL
+
+// <---------- Node.js vs Client side JS ----------->
+
+// window, document, DOM apis ( browser specific stuff) not present in node
+// node comes with built in modules that dont exist in the browser
+// these modules help to interact with os and files/folders
+// global object here is "global" (consists setTimeout, setInterval etc)
+// node index.js (in the terminal) executes index.js
+
+// <----------- Process and argv ------------------>
+
+// The process object provides information about, and control over, the current Node.js process.
+// The process object is an instance of EventEmitter.
+// it has bunch of functions .. process.version , process.release, process.cwd() (prints current working directory)
+// process.argv -> array fo strings
+// The process.argv property returns an array containing the command-line arguments passed when the Node.js process was
+// launched. The first element will be process.execPath. See process.argv0 if access to the original value of argv[0] is
+// needed.
+// The second element will be the path to the JavaScript file being executed. The remaining elements will be any additional
+// command-line arguments.
+
+// <-------------- File System Module ------------>
+
+// we have to do require('module_name') for using modules in node
+// all methods have async and sync versions
+// sync ones block the entire code halting all connections
+// fs.mkdir() -> async 
+// fs.mkdirSync() -> sync
+
+// const random = process.argv[2] || "Project" --> we can use this way if no arg is passed
+// fs.writeFileSync() to write in a file ..  creates if it doesnt exist / overwrite if present
+// these are created from where the script is ran
